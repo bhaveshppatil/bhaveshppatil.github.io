9f0f16fd7c3f1b20e7be33a31302297e50c03151
bhaveshppatil <bhavesh.patil0325@gmail.com>
2021-10-24 22:48:59 +0530

    first commit

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Bhavesh. I'm a Android developer based in Navi-Mumbai, Maharashtra, INDIA.",
+    description1: "My name's Bhavesh Patil. I'm a Android developer based in Navi-Mumbai, Maharashtra, INDIA.",
     description2: "A Aspiring full stack Android developer. Highly Skilled in designing, building & maintaining highly scalable and Unique Android applications. Ready to learn new technologies everyday and apply them through building new projects.",
     image: 2
 }
