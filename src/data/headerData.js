999625615d60ab8fc6db78072651080026685fe0
bhaveshppatil <bhavesh.patil0325@gmail.com>
2021-11-20 21:44:55 +0530

    npm

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Bhavesh Patil',
     title: "Android Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://github.com/bhaveshppatil/bhaveshppatil/blob/main/Social%20Icons/profile.jpg',
+    image: 'https://github.com/bhaveshppatil/bhaveshppatil/blob/main/Social%20Icons/profile.jpg?raw=true',
     resumePdf: resume
 }
