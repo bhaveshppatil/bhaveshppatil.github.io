a447b9c12d21f90a4fc4b26250db35df3c1e2516
bhaveshppatil <bhavesh.patil0325@gmail.com>
2021-11-29 18:33:31 +0530

    portfolio updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "About Me",
-    description1:"I have always found myself drawn to mobiles and computers and have tried to understand their working. This interest grew over the years and after completing my schooling, I decided to pursue a career in this field. After buying my first laptop, I started to learn about coding from various online sources. .",
+    description1:"I have always found myself drawn to mobiles and computers and have tried to understand their working. This interest grew over the years and after completing my schooling, I decided to pursue a career in this field. After buying my first laptop, I started to learn about coding from various online sources, within two months I was able to create my first location-based application.",
     description2:"In order to create a sustainable career, I realized the need for professional training and joined Masai School for a full-stack Android development course. Through my experiences here, I see myself growing as a better developer daily.",
     description3:"I have worked on interesting collaborative projects. I believe, that as an aspiring developer, the learning never stops and the professional journey is a series of challenging experiences.",
     description4:"I am passionate about building innovative projects and excelling in their development and look forward to growing with a creative organization. ",
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Bhavesh Patil',
     title: "Android Developer",
-    desciption:"I'm a passionate learner who's always willing to learn and work across technologies and domains 💡. I love to explore new technologies and leverage them to solve real-life problems ✨.",
+    desciption:"I’m an Android Developer, who's always willing to learn and work across new technologies and domains 💡✨.",
     image: 'https://github.com/bhaveshppatil/bhaveshppatil/blob/main/Social%20Icons/profile.jpg?raw=true',
     resumePdf: resume
 }
