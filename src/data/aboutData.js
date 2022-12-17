e0833bd0ebd9f4eed542628c37a0bef8316e3df1
bhaveshppatil <bhavesh.patil0325@gmail.com>
2022-12-17 21:51:09 +0530

    portfolio updated

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,10 +1,10 @@
 export const aboutData = {
     title: "About Me",
-    description1:"Hey there! My name is Bhavesh. I am an aspiring mobile app developer from Mumbai, India. I have experience in developing and designing Android and iOS applications with rich and intuitive user interfaces for approximately two years in the field.",
-    description2:"As part of a variety of team projects and individual projects over the past two years, I have designed and implemented 7+ individual projects from scratch on both Android & iOS platforms. In addition, I am familiar with the process of releasing apps on the Apple and Google Play stores.",   
-    description3:"My responsibilities included converting business needs into technical features and deploying the app. I have worked on MVC, MVP, and MVVM architectures, as well as SOLID principles and numerous design patterns, also well known to GitHub and Bitbucket software for code versioning and Jira software for project management.",  
-    description4:"A strong understanding of memory management & multithreading environments, as well as extensive experience with Firebase, Keychain, Room-DB, Core Location, MapKit, and other third-party frameworks. Built innovative apps across a variety of domains, including fintech, food, news, and lifestyle",
-    description5:"Having acquired all my skillset and toolset, I am looking forward to growing with one creative organization.",  
+    description1: "",
+    description2: "",
+    description3: "",
+    description4: "",
+    description5: "",
     image: 1
     
 }
